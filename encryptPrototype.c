4acd23877e050798b29f880ef1e9ed99cf94c91e
rmshimomura <shimomurarodrigo1@gmail.com>
2021-03-18 11:31:40 -0300

    Added GCD function and comments

--- encryptPrototype.c
+++ encryptPrototype.c
@@ -2,7 +2,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-long int myPower(long int base, long int exponent) {
+int gcd(int a, int b){ 
+    //Greatest common divisor
+    int result = 1;
+    int tempA = a;
+    int tempB = b;
+    for(int i = 2; i < 1000; i++){
+    int divs = 0;
+        for(int j = 2; j < i; j++){
+            if(i % j == 0 && i != j){
+                divs++;
+            }
+        }
+        if(divs < 1){
+            if(tempA % i == 0 && tempB % i == 0){
+                tempA /= i;
+                tempB /= i;
+                result *= i;
+                i--;
+                
+            }
+        }
+    }
+    return result;
+}
+
+long int myPower(long int base, long int exponent) { 
+    //My power replaces pow from math.h because of some problem with modulo operation
     long int result = 1;
     for (int i = 0; i < exponent; i++) {
         result *= base;
@@ -11,6 +37,7 @@ long int myPower(long int base, long int exponent) {
 }
 
 void findPrimes(long int* vectorOfPrimes, int numOfPrimes) {
+    //Find primes for encryption 
     int pos = 0;
     int primeNumber = 1;
     for (int i = 2; i < 1000; i++) {
@@ -32,7 +59,8 @@ void findPrimes(long int* vectorOfPrimes, int numOfPrimes) {
     }
 }
 
-long int findPublicKey(long int* vectorOfPrimes, int numOfPrimes) {
+long int findPublicKey(long int* vectorOfPrimes, int numOfPrimes) { 
+    //The first part of the public key that is the product of the primes
     long int N = 1;
     for (int i = 0; i < numOfPrimes; i++) {
         N *= vectorOfPrimes[i];
@@ -60,6 +88,20 @@ long int findInverseE(long int* vectorOfPrimes, int numOfPrimes, int e){
     }
 }
 
+long int findE(long int* vectorOfPrimes, int numOfPrimes){
+    //Find e, following the rules of the GDC between e and phiN needs to be 1
+    //Using brute force to find the smaller e that satisfies the requirements
+    int count = 2;
+    while(1){
+        if(gcd(count, phiN(vectorOfPrimes, numOfPrimes)) == 1){
+            break;
+        }else{
+            count++;
+        }
+    }
+    return count;
+}
+
 long int findPrivateKey(long int* vectorOfPrimes, int numOfPrimes, int e){
     return findInverseE(vectorOfPrimes, 3, e) % phiN(vectorOfPrimes, numOfPrimes);
 }
@@ -67,12 +109,12 @@ long int findPrivateKey(long int* vectorOfPrimes, int numOfPrimes, int e){
 
 int main() {
     long int v[3];
-    long int M = 75;
+    long int M = 123;
     findPrimes(v, 3);
     long int publicKey = findPublicKey(v,3);
-    long int e = 5;
+    long int e = findE(v, 3);
     long int inversee = findInverseE(v,3,e);
     long int privateKey = findPrivateKey(v,3,e);
-
+    printf("e = %ld inverse = %ld privatekey = %ld \n", e, inversee, privateKey);
     printf("result enc = %ld\n", myPower(M, e) % publicKey);
 }
